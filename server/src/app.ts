7e4208eb92076e3fa224d09bb603773fb91eebe9
yossely <yossely7@gmail.com>
2018-04-13 16:43:41 -0500

    Fetch and serve all hotels information
    Related #2

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,21 +1,52 @@
 import * as express from 'express';
 
+// Database
+const low = require('lowdb')
+const FileSync = require('lowdb/adapters/FileSync')
+const adapter = new FileSync('./database/data.json');
+
 class App {
-  public express
+  public express;
+  // Database handler
+  public db;
 
   constructor() {
-    this.express = express()
-    this.mountRoutes()
+    this.express = express();
+    this.initDatabase();
+    this.mountRoutes();
   }
 
+  /**
+   * Set all routes handle in the server
+   */
   private mountRoutes(): void {
-    const router = express.Router()
+    const router = express.Router();
+
+    // Get all hotels
+    router.get('/hotels', (req, res) => {
+      const hotels = this.db
+        .get('hotels')
+        .value();
+
+      res.json(hotels)
+    })
+
+    // Root route
     router.get('/', (req, res) => {
       res.json({
         message: 'Hello World!!'
       })
     })
-    this.express.use('/', router)
+
+    // Initialize router for the express application
+    this.express.use('/', router);
+  }
+
+  /**
+   * Initialize the database with the information in the json file
+   */
+  private initDatabase() {
+    this.db = low(adapter);
   }
 }
 
