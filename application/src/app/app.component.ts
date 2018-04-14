ab49a1518f329e811eec9d6779b2065beea0283c
yossely <yossely7@gmail.com>
2018-04-13 21:52:42 -0500

    Create basic mobile view
    - Navbar
    - Hotel image details card
      - Image
      - Name
      - Stars
      - Amenities
      - Price
      - _Ver hotel_ button
    
    Related #7

--- application/src/app/app.component.ts
+++ application/src/app/app.component.ts
@@ -1,5 +1,8 @@
 import { Component } from '@angular/core';
 
+import { DomSanitizer } from '@angular/platform-browser';
+import { MatIconRegistry } from '@angular/material';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +10,123 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
+
+  hotel = {
+    id: '249942',
+    name: 'Hotel Stefanos',
+    stars: 3,
+    price: 994.18,
+    image: '4900059_30_b.jpg',
+    amenities: [
+      'safety-box',
+      'nightclub',
+      'deep-soaking-bathtub',
+      'beach',
+      'business-center'
+    ]
+  };
+
+  constructor(private iconRegistry: MatIconRegistry, private sanitizer: DomSanitizer) {
+   this.registerAmenitiesIcons();
+  }
+
+  /**
+   * Register the amenities icons SVG's to be able to use them in mat-icons
+   */
+  registerAmenitiesIcons() {
+    // Bathrobes
+    this.iconRegistry.addSvgIcon(
+      'bath-robes',
+      this.sanitizer.bypassSecurityTrustResourceUrl('assets/icons/amenities/bathrobes.svg'));
+
+    // Bathtub
+    this.iconRegistry.addSvgIcon(
+      'bath-tub',
+      this.sanitizer.bypassSecurityTrustResourceUrl('assets/icons/amenities/bathtub.svg'));
+
+    // Beach
+    this.iconRegistry.addSvgIcon(
+      'beach',
+      this.sanitizer.bypassSecurityTrustResourceUrl('assets/icons/amenities/beach.svg'));
+
+    // Beach pool facilities
+    this.iconRegistry.addSvgIcon(
+      'beach-pool-facilities',
+      this.sanitizer.bypassSecurityTrustResourceUrl('assets/icons/amenities/beach-pool-facilities.svg'));
+
+    // Business center
+    this.iconRegistry.addSvgIcon(
+      'business-center',
+      this.sanitizer.bypassSecurityTrustResourceUrl('assets/icons/amenities/business-center.svg'));
+
+    // Children club
+    this.iconRegistry.addSvgIcon(
+      'children-club',
+      this.sanitizer.bypassSecurityTrustResourceUrl('assets/icons/amenities/children-club.svg'));
+
+    // Coffee maker
+    this.iconRegistry.addSvgIcon(
+      'coffe-maker',
+      this.sanitizer.bypassSecurityTrustResourceUrl('assets/icons/amenities/coffe-maker.svg'));
+
+    // Deep soaking bathtub
+    this.iconRegistry.addSvgIcon(
+      'deep-soaking-bathtub',
+      this.sanitizer.bypassSecurityTrustResourceUrl('assets/icons/amenities/deep-soaking-bathtub.svg'));
+
+    // Fitness center
+    this.iconRegistry.addSvgIcon(
+      'fitness-center',
+      this.sanitizer.bypassSecurityTrustResourceUrl('assets/icons/amenities/fitness-center.svg'));
+
+    // Garden
+    this.iconRegistry.addSvgIcon(
+      'garden',
+      this.sanitizer.bypassSecurityTrustResourceUrl('assets/icons/amenities/garden.svg'));
+
+    // Kitchen facilities
+    this.iconRegistry.addSvgIcon(
+      'kitchen-facilities',
+      this.sanitizer.bypassSecurityTrustResourceUrl('assets/icons/amenities/kitchen-facilities.svg'));
+
+    // Newspaper
+    this.iconRegistry.addSvgIcon(
+      'newspaper',
+      this.sanitizer.bypassSecurityTrustResourceUrl('assets/icons/amenities/newspaper.svg'));
+
+    // Nightclub
+    this.iconRegistry.addSvgIcon(
+      'nightclub',
+      this.sanitizer.bypassSecurityTrustResourceUrl('assets/icons/amenities/nightclub.svg'));
+
+    // Restaurant
+    this.iconRegistry.addSvgIcon(
+      'restaurant',
+      this.sanitizer.bypassSecurityTrustResourceUrl('assets/icons/amenities/restaurant.svg'));
+
+    // Safety box
+    this.iconRegistry.addSvgIcon(
+      'safety-box',
+      this.sanitizer.bypassSecurityTrustResourceUrl('assets/icons/amenities/safety-box.svg'));
+
+    // Separate-bredroom
+    this.iconRegistry.addSvgIcon(
+      'separate-bredroom',
+      this.sanitizer.bypassSecurityTrustResourceUrl('assets/icons/amenities/separate-bredroom.svg'));
+
+    // Sheets
+    this.iconRegistry.addSvgIcon(
+      'sheets',
+      this.sanitizer.bypassSecurityTrustResourceUrl('assets/icons/amenities/sheets.svg'));
+
+  }
+
+  /**
+   * Get an array which length is the number of stars to use it in an ngFor directive
+   *
+   * @returns {Array<number>}
+   */
+  numberOfStars(): Array<number> {
+    return new Array(this.hotel.stars).fill(this.hotel.stars);
+  }
 }
--- application/src/app/app.module.ts
+++ application/src/app/app.module.ts
@@ -1,16 +1,33 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
 
+// Material
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatExpansionModule } from '@angular/material/expansion';
+import { MatCardModule } from '@angular/material/card';
+import { MatIconModule } from '@angular/material/icon';
+import { MatDividerModule } from '@angular/material/divider';
+import { MatButtonModule } from '@angular/material/button';
 
 import { AppComponent } from './app.component';
-
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule,
+    // Material
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatExpansionModule,
+    MatCardModule,
+    MatIconModule,
+    MatDividerModule,
+    MatButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
